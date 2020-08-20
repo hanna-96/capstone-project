fa4c012593c549e03d32a5d373d1adef9ed7d67d
Hanna Rzheutskaya <a.rzheutskaya96@gmail.com>
2020-08-20 17:54:28 -0400

    connected dynamoDb to node

--- server/dynamoDB.js
+++ server/dynamoDB.js
@@ -1,7 +1,13 @@
-const AWS = require("aws-sdk");
-AWS.config.update({
+var AWS = require("aws-sdk");
+const {accessKeyId,secretAccessKey} = require('../secrets')
+let awsConfig = {
   region: "us-east-2",
-});
+  endpoint: "http://dynamodb.us-east-2.amazonaws.com",
+  accessKeyId,
+  secretAccessKey
+};
+AWS.config.update(awsConfig);
+
 const DynamoDB = new AWS.DynamoDB();
 
 async function createTable() {
@@ -9,21 +15,21 @@ async function createTable() {
     TableName: "Users",
     KeySchema: [
       { AttributeName: "id", KeyType: "HASH" },
-      { AttributeName: "userName", KeyType: "HASH" },
-      { AttributeName: "firstName", KeyType: "HASH" },
-      { AttributeName: "lastName", KeyType: "HASH" },
-      { AttributeName: "id", KeyType: "HASH" },
-      { AttributeName: "id", KeyType: "HASH" },
-      { AttributeName: "id", KeyType: "HASH" },
+      // { AttributeName: "userName", KeyType: "HASH" },
+      // { AttributeName: "firstName", KeyType: "HASH" },
+      // { AttributeName: "lastName", KeyType: "HASH" },
+      // { AttributeName: "email", KeyType: "HASH" },
+      // { AttributeName: "password", KeyType: "HASH" },
+      // { AttributeName: "cabinet", KeyType: "RANGE" },
     ],
     AttributeDefinitions: [
-      { AttributeName: "id", AttributeType: "S" },
-      { AttributeName: "id", AttributeType: "S" },
-      { AttributeName: "id", AttributeType: "S" },
-      { AttributeName: "id", AttributeType: "S" },
-      { AttributeName: "id", AttributeType: "S" },
-      { AttributeName: "id", AttributeType: "S" },
-      { AttributeName: "id", AttributeType: "S" },
+      { AttributeName: "id", AttributeType: "N" },
+      // { AttributeName: "userName", AttributeType: "S" },
+      // { AttributeName: "firstName", AttributeType: "S" },
+      // { AttributeName: "lastName", AttributeType: "S" },
+      // { AttributeName: "email", AttributeType: "S" },
+      // { AttributeName: "password", AttributeType: "S" },
+      // { AttributeName: "cabinet", AttributeType: "M" }, // M=object ; L= array;what is better
     ],
     ProvisionedThroughput: {
       ReadCapacityUnits: 10,
@@ -32,3 +38,26 @@ async function createTable() {
   };
   return await DynamoDB.createTable(params).promise();
 }
+(async ()=>{
+  console.log('the func call', await createTable());
+})()
+
+// function addItem(id, userName, firstName, lastName,) {
+//   const params = {
+//     TableName: "Users",
+//     Item: {
+//       id: { S: id },
+//       name: { S: name },
+//       image: { S: image },
+//       description: { S: description },
+//     },
+//   };
+
+//  return await  DynamoDB.putItem(params, async function (err) {
+//     if (err) {
+//       console.error("Unable to add a spot", err);
+//     } else {
+//      await console.log(`Added ${name} to the database`);
+//     }
+//   }.promise());
+// }
