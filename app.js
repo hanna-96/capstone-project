b40c0e4019494b307cffb78b6f91184f471d9ff5
Hanna Rzheutskaya <a.rzheutskaya96@gmail.com>
2020-08-20 12:37:37 -0400

    added gitignore

---
+++ app.js
@@ -0,0 +1,8 @@
+    // //  Register service worker.
+    // if ('serviceWorker' in navigator) {
+    //     // Use the window load event to keep the page load performant
+    //       navigator.serviceWorker.register('./service-worker.js')
+    //       .then((reg)=>console.log("Servicesworker is registered!",reg)
+    //       .catch((err)=>console.log('serviceworker is NOT registered',err)));//not sure about path to serversworker
+        
+    //   }
