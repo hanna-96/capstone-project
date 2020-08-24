322d93c43e51de1095440366574d2560406a8108
Sara Culhane <sjcully@mac.com>
2020-08-24 10:57:54 -0500

    fixed: css-loaders

--- request-test.js
+++ request-test.js
@@ -31,7 +31,7 @@ const Request = () => {
   return (
     
     <div>
-      {console.log(drinks)}
+      {/* {console.log(drinks)} */}
       { drinks.length ? <div>
         <p><img src={drinks[0].strDrinkThumb} /></p>
         <p>Drink of the Day : {drinks[0].strDrink}</p>
--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,19 @@ module.exports = env => {
           test: /\.jsx?$/,
           exclude: /node_modules/,
           loader: 'babel-loader'
-        }]},
+        },
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            // Creates `style` nodes from JS strings
+            'style-loader',
+            // Translates CSS into CommonJS
+            'css-loader',
+            // Compiles Sass to CSS
+            'sass-loader'
+          ]
+        }],
+      },
     plugins: [
     new webpack.HashedModuleIdsPlugin(),
     new HtmlWebpackPlugin({
