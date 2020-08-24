dbc558df907c948e1fec082647853358ea0a9ea0
Sara Culhane <sjcully@mac.com>
2020-08-24 08:05:30 -0500

    feat: react integration

--- app.js
+++ app.js
@@ -1,45 +1,17 @@
-// //  Register service worker.
-// if ("serviceWorker" in navigator){
-//     window.addEventListener("load", () => {
-//     navigator.serviceWorker.register("/public/sw.js");
-//     })
-// }
-import { Workbox } from "workbox-window";
-fetch("https://api.exchangeratesapi.io/latest")
-.then(response => response.json())
-.then(data => {
-  const main = document.querySelector("#main");
-  if (!data || !data.rates) {
-    main.innerHTML = "There was an error. Please try again.";
-    return false;
-  }
-  let html = "";
-  for (const [currency, rate] of Object.entries(data.rates)) {
-    html += `<article class="card card-currency">
-    <div class="currency">${currency}</div>
-    <div class="rate">${rate}</div>
-    </article>`;
-  }
-  main.innerHTML = html;
-});
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    const wb = new Workbox("/public/sw.js");
-    const updateButton = document.querySelector("#app-update");
-    // Fires when the registered service worker has installed but is waiting to activate.
-    wb.addEventListener("waiting", event => {
-      updateButton.classList.add("show");
-      updateButton.addEventListener("click", () => {
-      // Set up a listener that will reload the page as soon as the previously waiting service worker has taken control.
-      wb.addEventListener("controlling", event => {
-        window.location.reload();
-      });
-      // Send a message telling the service worker to skip waiting.
-      // This will trigger the `controlling` event handler above.
-      wb.messageSW({ type: "SKIP_WAITING" });
-    });
-    });
-    wb.register();
-  });
+import React from 'react';
+import Request from './request-test'
+
+// import './App.css';
+// import Routes from './routes'
+// import NavBar from './components/NavBar'
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Welcome to Mixologist</h2>
+      <Request />
+    </div>
+  );
 }
 
+export default App;
