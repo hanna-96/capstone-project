96cadf807d640e6cea1f7d7f6f4098064509ad46
Sara Culhane <sjcully@mac.com>
2020-08-25 08:22:03 -0500

    feat: will skip an ingredient if it is not found

--- client/request-test.js
+++ client/request-test.js
@@ -3,72 +3,57 @@ import React, {useEffect, useState} from 'react'
 import RequestId from './request-id'
 
 const Request = () => { 
-
+  const allDrinks = []
   const [drinkList, setDrinks] = useState([])
 
-  const [didRun, setDidRun] = useState(false)
+
   const [len, setLen] = useState([])
-  const allDrinks = []
-  const ingreds = ['lime_juice', 'tequila', 'gin', 'vodka']
+  
+  const [skip, setSkip] = useState(0)
+  const ingreds = ['lime_juice', 'sauce', 'tequila', 'gin', 'vodka']
 
   useEffect( () => {
     
-    // const getDrinks = async ()  => {
-      
-    //   try {
-    //     ingreds.forEach( async (ing, idx) => {
-    //       const {data}= await axios.get(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${ing}`);
-    //       const {drinks} = data
-    //       allDrinks.push(...drinks)
-          
-    //       if(idx === ingreds.length-1 ) {
-    //         setDrinks(allDrinks)
-    //         setLen(allDrinks.length)
-    //       }
-    //     }) 
-    //     // const {data} =await axios.get('https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=lime_juice')
-    //     // const {drinks} = data
-    //   } catch (error) {
-    //     console.log(error);
-    //   }
-
-    // }
     
     const getDrinks = async (ing)  => {
       
       try {
-        const {data} =await axios.get(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${ing}`)
-        const {drinks} = data
-        setDrinks(prevDrinks => [...prevDrinks,...drinks])
-        setLen(prevLen => [...prevLen, drinks.length ])
+        const {data} =await axios.get(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${ing}`) || ''
+        if(typeof data !== 'string') {
+          const {drinks} = data
+          setDrinks(prevDrinks => [...prevDrinks,...drinks])
+          setLen(prevLen => [...prevLen, drinks.length ])
+        }
+        else {
+          setDrinks(prevDrinks => [...prevDrinks])
+          setLen(prevLen => [...prevLen])
+          setSkip(idx => idx+1)
+          
+        }
       } catch (error) {
         console.log(error);
       }
-
+      
     }
-    ingreds.forEach( async ing => await getDrinks(ing))
+   
+    ingreds.forEach( async (ing) => await getDrinks(ing))
 
- 
-    
-  //   getDrinks()
-    
-  //   setDidRun(true)
-  // }
 }, [])
 
-
+ if (skip) {ingreds.splice(skip,1)}
   return (
     
     
     <div>
+      {console.log(len, skip)}
       { 
+
       drinkList.length && len.length === ingreds.length ?
       <div>
-        {console.log(drinkList, 'the drinks', len)}
         <p><img src={drinkList[140].strDrinkThumb} /></p>
         <h1>hi</h1>
         <p>Drink of the Day : {drinkList[0].strDrink}</p>
-        {/* <RequestId id={drinkList[0].idDrink}/> */}
+        {console.log(drinkList[140])}
         </div>
  : <div> Loading...
    </div>} 
