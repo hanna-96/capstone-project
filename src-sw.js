b8d2afd015485966762df396499cdbb72ef9aef4
Hanna Rzheutskaya <a.rzheutskaya96@gmail.com>
2020-08-26 15:02:49 -0400

    fixed signUp

--- src-sw.js
+++ src-sw.js
@@ -2,14 +2,14 @@
 workbox.precaching.precacheAndRoute(self.__precacheManifest);
 //^^^^ this is from smashing magazine tutorial
 
-workbox.routing.registerRoute(
-  /https:\/\/api\.exchangeratesapi\.io\/latest/,
-  new workbox.strategies.NetworkFirst({
-    cacheName: "currencies",
-    plugins: [
-      new workbox.expiration.Plugin({
-        maxAgeSeconds: 10 * 60 // 10 minutes
-      })
-    ]
-  })
-);
+// workbox.routing.registerRoute(
+//   /https:\/\/api\.exchangeratesapi\.io\/latest/,
+//   new workbox.strategies.NetworkFirst({
+//     cacheName: "currencies",
+//     plugins: [
+//       new workbox.expiration.Plugin({
+//         maxAgeSeconds: 10 * 60 // 10 minutes
+//       })
+//     ]
+//   })
+// );
