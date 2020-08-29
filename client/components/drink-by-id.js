6c89e2cb240a4a532d7396837233a1008d25a13b
Sara Culhane <sjcully@mac.com>
2020-08-29 15:37:05 -0500

    updated drink detail page

--- client/components/drink-by-id.js
+++ client/components/drink-by-id.js
@@ -7,7 +7,9 @@ const DrinkId = (props) => {
     const id = history.state.state.id
     console.log(props)
     const [drinkDetails, setDrink] = useState({})
-    const [ingred, setIngreds] = useState([])
+    const [ingreds, setIngreds] = useState([])
+    const [measure, setMeasure] = useState([])
+    const [details, setDetails] = useState([])
     const [didRun, setDidRun] = useState(false)
 
     const results = () => {
@@ -36,20 +38,37 @@ const DrinkId = (props) => {
       
     } )
 
-    // const getIngredients = () => {
-    //   const drinkProps = Object.keys(drinkDetails)
-    //   drinkProps.forEach((prop) => {
-    //     if (prop.includes('strIngredient') && drinkProps[prop] !== null) setIngreds(prev => [...prev, drinkProps[prop]])
-    //   })
-    // }
+    useEffect( () => {
+      const getIngAndMeasure = () => {
+        const drinkProps = Object.keys(drinkDetails)
+        const ingredients = drinkProps.filter((prop) => prop.includes('strIngredient') && drinkDetails[prop] !== null).map((prop) => drinkDetails[prop])
+        const measurements = drinkProps.filter((prop) => prop.includes('strMeasure') && drinkDetails[prop] !== null).map((prop) => drinkDetails[prop])
+        setIngreds(ingredients)
+        setMeasure(measurements)
+
+        for (let i = 0; i < ingredients.length ; i++) {
+          setDetails(prev => [...prev, {ingreds: ingredients[i], measure: measurements[i]}])
+        }
+
+      }
+      getIngAndMeasure()
+  
+    }, [drinkDetails]
+    )
+  
   
     return (
     
         <div>
-            { didRun ? console.log(drinkDetails ,'the data') : null}
+            { didRun ? console.log('details', details) : null}
           { drinkDetails.strDrink ? <div>
             <p>{drinkDetails.strDrink}</p>
-            <p>{drinkDetails.strMeasure1}</p>
+            <p><img src={drinkDetails.strDrinkThumb}/></p>
+
+            <h2>Receipe</h2>
+
+            {details.map(detail  =><p>{detail.ingreds} {detail.measure}</p> )}
+            <p>{drinkDetails.strInstruction}</p>
             <Button onClick={results}>Back to results</Button>
             </div>
      : <div></div>} 
