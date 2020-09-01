4ad7f26690d002068e258b6e9d63193f5750d21c
lialjohn <sasabonsam@gmail.com>
2020-09-01 06:26:49 +0100

    feat: frontend routes that are not the landing page now cache

--- src-sw.js
+++ src-sw.js
@@ -1,1 +1,27 @@
 workbox.precaching.precacheAndRoute(self.__precacheManifest);
+
+//for testing cache
+workbox.routing.registerRoute(
+  /http:\/\/localhost:8080\//,
+  new workbox.strategies.NetworkFirst({
+    cacheName: "testCache",
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxAgeSeconds: 10 * 60 // 10 minutes
+      })
+    ]
+  })
+)
+
+//Liz's deployed heroku address
+workbox.routing.registerRoute(
+  /https:\/\/mixologist-cap\.herokuapp\.com\//,
+  new workbox.strategies.NetworkFirst({
+    cacheName: "mixology",
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxAgeSeconds: 259200 //3 days
+      })
+    ]
+  })
+)
